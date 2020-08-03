0bf050d021e1c589f871372766e4774a1d050a71
Ketki Pandey <59409021+skywalker-its@users.noreply.github.com>
2020-08-03 15:21:23 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,55 @@
+class Food {
+    constructor() {
+       this.foodStock = null
+       this.lastFed = null
+       this.bottleimg = loadImage("images/Milk.png")
+    }
+
+    getFS(){
+       var foodSref = db.ref('/Food')
+       foodSref.on("value", (data) =>{
+       this.foodStock = data.val(); 
+     })
+  }
+     
+    getLF(){
+       var foodSref = db.ref('/LastFedTime')
+       foodSref.on("value", (data) =>{
+       this.lastFed = data.val(); 
+     })
+  } 
+
+  updateLF(){
+    var foodSref = db.ref('/') 
+    foodSref.update({
+        'LastFedTime': hour + period
+
+    })
+ }
+
+deductF(){
+    var foodSrefs = db.ref('/') 
+    foodSrefs.update({
+        'Food': this.foodStock - 1
+    })
+  }
+  display(){
+    var x = 80
+    var y = 100
+    imageMode(CENTER);
+    if(this.foodStock > 0){
+        
+        for(var i = 0; i < this.foodStock; i++){
+              if(i%10 === 0){
+                x = 80
+                y = y + 50
+            }
+            
+            image(this.bottleimg, x, y , 50, 50)
+            x = x + 30
+        }
+    }
+    
+    }
+    
+} 
---
+++ sketch.js
@@ -0,0 +1,91 @@
+//var database ;
+const db = firebase.database();
+var dog, happydDog, foodS, foodStock;
+var stockbutton
+var dogimg, happydogimg;
+var food
+var hour
+var period = ""
+
+function preload(){
+  //load images here
+  dogimg = loadImage("images/dogImg.png");
+  dogimg2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  food = new Food;
+  food.getFS
+  canvas = createCanvas(800, 800);
+  dog = createSprite (575, 250,1,1);
+  dog.scale = 0.15;
+  dog.addImage(dogimg, "dogimg");
+
+  stockbutton = createButton("Add Stock");
+  stockbutton.mousePressed(stock);
+  stockbutton.position(100,100)
+  
+  feadbutton = createButton("Feed");
+  feadbutton.mousePressed(feed);
+  feadbutton.position(200,100)
+
+} 
+ 
+
+function draw() {  
+  background(46, 139, 87);
+  drawSprites();
+  food.getFS();
+  food.getLF();
+
+  getFeedTime();
+ /// foodStock = db.ref('Food');
+ /// foodStock.on('value', readStock);
+  stroke("white");
+  text("Food Remaing: "+ food.foodStock, 50, 100);
+  
+  text("Last Fed: "+ food.lastFed, 50, 80 )
+  //text("Remember the Up arrow, will feed the dog.",50, 150);
+  food.display();
+// if(frameCount % 10 === 0){
+   //  stock();
+// }
+}
+
+//function readStock(data){
+//    foodS = data.val();
+//    //console.log("hello");
+//}
+function stock(){
+  food.updateFS();
+  
+  console.log("It be working")
+}
+function feed(){
+  food.deductF();
+  food.updateLF()
+  if(hour > 12){
+      period = "PM"
+  }
+  if(hour < 12){
+      period = "AM"
+  }
+  console.log("It be working")
+
+}
+
+//function writeStock(food){
+//
+//    firebase.database().ref("/").update({
+//        Food: food
+//    })
+//}
+async function getFeedTime(){
+  var response = await fetch("https://worldtimeapi.org/api/timezone/America/Los_Angeles");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  hour = datetime.slice(11,13);
+  
+  console.log(hour)
+}
